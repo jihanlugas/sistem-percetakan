c97ca93e101958cc6cf1daf1bf284a93683f608b
Jihan Lugas <jihanlugas2@gmail.com>
2026-06-22 10:50:19 +0700

    change package generate PDF

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.13.2
+	github.com/labstack/gommon v0.4.2
+	github.com/playwright-community/playwright-go v0.5700.1
 	github.com/spf13/cobra v1.8.1
 	github.com/swaggo/echo-swagger v1.4.1
 	github.com/swaggo/swag v1.16.4
@@ -20,21 +22,17 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
-	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.9.3 // indirect
-	github.com/chromedp/cdproto v0.0.0-20250216233945-bd41ad9b04ce // indirect
-	github.com/chromedp/chromedp v0.12.1 // indirect
-	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.8.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/gobwas/httphead v0.1.0 // indirect
-	github.com/gobwas/pool v0.2.1 // indirect
-	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -43,16 +41,11 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/jung-kurt/gofpdf v1.16.2 // indirect
-	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/phpdave11/gofpdi v1.0.14 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
-	github.com/signintech/gopdf v0.29.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- utils/pdf.go
+++ utils/pdf.go
@@ -1,59 +1,108 @@
 package utils
 
 import (
-	"context"
 	"fmt"
-	"github.com/chromedp/cdproto/page"
-	"github.com/chromedp/chromedp"
+	"net/url"
 	"os"
+	"path/filepath"
 	"time"
+
+	"github.com/labstack/gommon/log"
+	"github.com/playwright-community/playwright-go"
 )
 
-// Generate PDF menggunakan chromedp dan cdproto/page
+// GeneratePDFWithChromedp masih dipertahankan nama fungsi untuk kompatibilitas,
+// tapi sekarang menggunakan Playwright untuk menghasilkan PDF.
 func GeneratePDFWithChromedp(inputHTML string) ([]byte, error) {
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
-	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
-	defer cancel()
-
-	fullPath, err := os.Getwd()
+	absPath, err := filepath.Abs(inputHTML)
 	if err != nil {
+		log.Print("Err filepath.Abs ", err)
 		return nil, err
 	}
 
-	htmlFile := fmt.Sprintf("file://%s/%s", fullPath, inputHTML)
-	var pdfBuffer []byte
+	if _, err := os.Stat(absPath); err != nil {
+		log.Print("Err os.Stat ", err)
+		return nil, fmt.Errorf("input HTML file not found: %w", err)
+	}
 
-	var contentHeight float64
+	fileURL := (&url.URL{
+		Scheme: "file",
+		Path:   filepath.ToSlash(absPath),
+	}).String()
 
-	// Menggunakan cdproto/page untuk generate PDF
-	err = chromedp.Run(ctx,
-		chromedp.Navigate(htmlFile),
-		chromedp.WaitReady("body", chromedp.ByQuery),
-		chromedp.Evaluate(`document.body.scrollHeight`, &contentHeight),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			const pxPerInch = 96.0
-			heightInInch := contentHeight / pxPerInch
-			buf, _, err := page.PrintToPDF().
-				WithPaperWidth(5.9).
-				WithPaperHeight(heightInInch).
-				WithMarginTop(0).
-				WithMarginBottom(0).
-				WithMarginLeft(0).
-				WithMarginRight(0).
-				WithPreferCSSPageSize(false).
-				Do(ctx)
-			if err != nil {
-				return err
-			}
-			pdfBuffer = buf
-			return nil
-		}),
-	)
+	// Start Playwright
+	pw, err := playwright.Run()
 	if err != nil {
+		log.Print("playwright.Run ", err)
+		return nil, err
+	}
+	defer func() {
+		_ = pw.Stop()
+	}()
+
+	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+		Headless: playwright.Bool(true),
+		Args:     []string{"--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"},
+	})
+	if err != nil {
+		log.Print("browser.Launch ", err)
+		return nil, err
+	}
+	defer func() {
+		_ = browser.Close()
+	}()
+
+	page, err := browser.NewPage()
+	if err != nil {
+		log.Print("browser.NewPage ", err)
 		return nil, err
 	}
 
-	return pdfBuffer, nil
+	// Navigate to the file URL
+	if _, err = page.Goto(fileURL, playwright.PageGotoOptions{WaitUntil: playwright.WaitUntilStateNetworkidle}); err != nil {
+		log.Print("page.Goto ", err)
+		return nil, err
+	}
+
+	// Give the page a short moment to settle
+	time.Sleep(300 * time.Millisecond)
+
+	// Get content height
+	heightVal, err := page.Evaluate("() => document.body.scrollHeight")
+	if err != nil {
+		log.Print("page.Evaluate height ", err)
+		return nil, err
+	}
+
+	var heightPx int
+	switch v := heightVal.(type) {
+	case float64:
+		heightPx = int(v)
+	case int:
+		heightPx = v
+	default:
+		heightPx = 0
+	}
+	if heightPx <= 0 {
+		heightPx = 1122 // fallback height
+	}
+
+	// Generate PDF using Playwright. Use pixel height to match content length.
+	pdfBuf, err := page.PDF(playwright.PagePdfOptions{
+		PrintBackground: playwright.Bool(true),
+		Width:           playwright.String("5.9in"),
+		Height:          playwright.String(fmt.Sprintf("%dpx", heightPx)),
+		Margin: &playwright.Margin{
+			Top:    playwright.String("0in"),
+			Bottom: playwright.String("0in"),
+			Left:   playwright.String("0in"),
+			Right:  playwright.String("0in"),
+		},
+	})
+	if err != nil {
+		log.Print("page.PDF ", err)
+		return nil, err
+	}
+
+	return pdfBuf, nil
 }
